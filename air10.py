b2cf4a1fe299379e7a14c4f305564d106f47c65c
NN-Guillaume <neebnicoli.guillaume@gmail.com>
2023-05-09 11:48:54 +0200

    10 is working

--- air10.py
+++ air10.py
@@ -2,3 +2,26 @@
 
 # Créer un programme qui affiche le contenu d'un fichier donné en argument
 
+import sys
+
+def readFile(argFile):
+
+    # Opening the given file in read-only mode
+    with open(argFile, 'r') as fileData:
+        
+        # Go through each line of the file
+        for line in fileData:
+            
+            # Print the line
+            print(line)
+
+    # Always close the file after reading it
+    fileData.close()
+
+try:
+    # Give the file in argument
+    inputFile = sys.argv[1]
+except:
+    print(" ERROR ")
+
+readFile(inputFile)
