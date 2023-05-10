c77a940af209096dee0f1d312fbd98caad714bd0
NN-Guillaume <neebnicoli.guillaume@gmail.com>
2023-05-10 18:50:00 +0200

    4 is working

--- air04.py
+++ air04.py
@@ -2,43 +2,49 @@
 
 # Créer un programme qui affiche une  chaîne de caractères en évitant les caractères identiques adjacents.
 
-# exemple ---> " Hello my lord "
+# exemple ---> "Hello my lord"
 #  ---> " Helo my lord "
 import sys
 
-# Look for twin char
-"""def huntTwin(li):
-    
-    abc = 'abcdefghijklmnopqrstuvwxyz'
-    abc = list(abc)
-    
-    for x in li:
-        for y in abc:
-            if y == x*2:
-                print("TWIN")
-            else:
-                print(" ??? ")"""
 
-def comp(li, arg):
-    rejectList = [ x for x in li if arg in x]
-    print(rejectList)
+# turn the list into a string and display it
+def returnToString(li):
+    simpleStr = ' '.join(map(str, li))
+    #print(simpleStr, end=' ')
+    print(simpleStr, end=' ')
+
+
+# revert to a simple list
+def revertBack(li):
+    li[0::] = [''.join(li[0::])]
+    #print(li)
+    returnToString(li)
+
+
+# take care of any twin letters
+def twinHunter(li):
+    li = list(dict.fromkeys(li))
+    #return(li)
+    #print(li)
+    revertBack(li)
+
 
 # Convert each word into a list
 def wordConvert(li):
     for word in li:
-        #print(i, sep='\n')
         isList = list(word)
-        #print(*isList, sep='\n')
-        print(isList, sep='\n')
-        
-        
+        #print(isList, sep='\n')
+        twinHunter(isList)
+
 
 # Turn your input into a list
 def strToList(sentence):
     nowList = list(sentence.split(" "))
-    print(nowList)
+    #print(nowList)
     wordConvert(nowList)
 
+
 myInput = sys.argv[1]
+
 
 strToList(myInput)
